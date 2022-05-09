d26db13e1037e4ac6777d13c146be7991921ee66
Shi-Chin <fred3571588@gmail>
2022-05-09 17:32:46 +0800

    feat:5/9

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,6 +6,7 @@ use App\Interfaces\OrderRepositoryInterface;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 
 class OrderController extends Controller
 {
@@ -25,11 +26,34 @@ class OrderController extends Controller
 
     public function store(Request $request): JsonResponse
     {
+        // $validated = $request->validate([
+        //     'client' => ['required','unique:orders','max:5'],
+        //     'details' => ['required'],
+        // ]);
+
+        $validator = Validator::make($request->all(), [
+            'client' => 'required|unique:orders|max:255',
+            'details' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/api/order/')
+                        ->withErrors($validator)
+                        ->withInput();
+        }
+
+        // Retrieve the validated input...
+        $validated = $validator->validated();
+
+        // Retrieve a portion of the validated input...
+        $validated = $validator->safe()->only(['client', 'details']);
         $orderDetails = $request->only([
             'client',
             'details'
         ]);
 
+        $data = $request->session->all();
+        
         return response()->json(
             [
                 'data' => $this->orderRepository->createOrder($orderDetails)
