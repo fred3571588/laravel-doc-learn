f4456f1667c2df0a3f186f575618192cab861871
Shi-Chin <fred3571588@gmail>
2022-05-13 15:21:23 +0800

    feat:relation文件學習

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -9,13 +12,28 @@ class UserController extends Controller
     //
     public function index(Request $request)
     {
-        # code...
+        $user = User::find(1);
+        $user->posts()
+            ->where(function (Builder $query) {
+                return $query->where('active', 1)
+                            ->orWhere('vote', '>=', 100);
+            })
+            ->get();
+
+
+
+        $books = Book::with('author')->get();
+
+        foreach ($books as $book){
+            echo $book->author->name;
+        }
+
         return view('welcome');
     }
 
     public function show(Request $request)
     {
-        $value = $request->session()->get('key','default');
+        $value = $request->session()->get('key', 'default');
         dd($value);
     }
 }
